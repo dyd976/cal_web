ec250f3f0a9dc7ac8929dab25566a1630ff155ed
dyd976 <dyd976@gmail.com>
2025-04-19 22:19:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,15 @@ function calculate() {
     let rareEarth2 = parseInt(document.getElementById('rareEarth2').value) || 0;
     let rareEarth3 = parseInt(document.getElementById('rareEarth3').value) || 0;
 
-    let rareEarth1Gold = parseInt(document.getElementById('rareEarth1Gold').value) || 0;
-    let rareEarth2Gold = parseInt(document.getElementById('rareEarth2Gold').value) || 0;
-    let rareEarth3Gold = parseInt(document.getElementById('rareEarth3Gold').value) || 0;
-
     // 화기연료
     let fireFuel1 = parseInt(document.getElementById('fireFuel1').value) || 0;
     let fireFuel2 = parseInt(document.getElementById('fireFuel2').value) || 0;
     let fireFuel3 = parseInt(document.getElementById('fireFuel3').value) || 0;
 
-    let fireFuel1Gold = parseInt(document.getElementById('fireFuel1Gold').value) || 0;
-    let fireFuel2Gold = parseInt(document.getElementById('fireFuel2Gold').value) || 0;
-    let fireFuel3Gold = parseInt(document.getElementById('fireFuel3Gold').value) || 0;
-
     // 생명주철
     let lifeIron1 = parseInt(document.getElementById('lifeIron1').value) || 0;
     let lifeIron2 = parseInt(document.getElementById('lifeIron2').value) || 0;
-    let lifeIron3 = parseInt(document.getElementById('lifeIron3
+    let lifeIron3 = parseInt(document.getElementById('lifeIron3').value) || 0;
+
+    // 운철파편
+    let meteorShard1 = parseInt(
